43d0a5824a77ec06d9e0867f194ce9b656d7993e
RoddyWiertsema <itsdigits@gmail.com>
2023-12-29 00:28:18 +0100

    Setup preliminary imports and constants

--- src/rsp.c
+++ src/rsp.c
@@ -1,3 +1,15 @@
 //
 // Created by might on 28/12/2023.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+// Constants for later use
+#define MAX_LISTEN_BACKLOG 1
+#define BUFFER_SIZE 4096
