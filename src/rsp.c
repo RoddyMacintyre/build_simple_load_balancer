f04f3d9d702dcf8220ca3e287cd7c679baade1d0
RoddyWiertsema <itsdigits@gmail.com>
2024-01-02 20:50:11 +0100

    Create a server listening out for connections

--- src/rsp.c
+++ src/rsp.c
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
 
 // Constants for later use
 #define MAX_LISTEN_BACKLOG 1
@@ -87,4 +88,127 @@ void handle_client_connection(int client_socket_file_descriptor,    // low level
     }
     // Close client socket
     close(client_socket_file_descriptor);
+}
+
+// Create a main func
+int main(int argc, char *argv[]){
+    printf("In main...\n");
+    // Start off defining local vars
+    char *server_port_string;
+    char *backend_address;
+    char *backend_port_string;
+
+    struct addrinfo hints;
+    struct addrinfo *address;
+    struct addrinfo *address_iterator;
+
+    int getaddrinfo_error;
+
+    int server_socket_file_descriptor;
+    int client_socket_file_descriptor;
+
+    int so_reuseaddress;
+
+    printf("Done with initialization of vars...\n");
+
+    // Check if we have the right amount of cmd args adn assign them to vars
+    if (argc != 4) {
+        printf("Not enough arguments");
+
+        fprintf(stderr,
+                "Usage: %s  \n",
+                argv[0]     // prints out "/mnt/d/Career/programming_Education/build_load_balancer_c/rsp"
+                );
+        exit(1);
+    }
+
+    printf("Caught correct number of args...\n");
+
+    server_port_string = argv[1];
+    backend_address = argv[2];
+    backend_port_string = argv[3];
+
+//    printf("Vars inspection:\n%s\n%s\n%s\n",
+//           server_port_string,
+//           backend_address,
+//           backend_port_string);
+
+    // Next step: get address of localhost in a similar fashion to the client connection.
+    // But add 1 extra value to the hints & pass NULL as the first param
+    memset(&hints, 0, sizeof(struct addrinfo));
+//    memset(&hints, 0, sizeof hints);
+    printf("Memory set...\n");      // Cannot use printf with only a string. Needs some form of an expression! Be aware.
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    printf("Hints defined...\n");
+
+    getaddrinfo_error = getaddrinfo(NULL, server_port_string, &hints, &address);
+
+    // AI_PASSIVE combined with NULL tells getaddrinfo we want to run a serv socket on this address
+    // Able to listen for incoming conns, accept them and handle them
+    // Once we have the list, we iterate through them, for each create a socket like before,
+    // but instead of connecting, we bind so we can accept incoming conns
+    for (address_iterator = address; address_iterator != NULL; address_iterator = address_iterator->ai_next){
+        printf("Trying to define a file descriptor...\n");
+        server_socket_file_descriptor = socket(address_iterator->ai_family,
+                                               address_iterator->ai_socktype,
+                                               address_iterator->ai_protocol);
+
+        if(server_socket_file_descriptor == -1){
+            continue;
+        }
+        printf("Caught a legit address!\n");
+        so_reuseaddress = 1;
+        setsockopt(server_socket_file_descriptor,
+                   SOL_SOCKET,
+                   SO_REUSEADDR,    // Share the socket to mitigate ctrl-c exits and subsequent resource alloc lag
+                   &so_reuseaddress,
+                   sizeof(so_reuseaddress));
+
+        // Bind says "I own socket, and I'm listening for incoming conns"
+        if(bind(server_socket_file_descriptor,
+                address_iterator->ai_addr,
+                address_iterator->ai_addrlen) == 0){
+            break;
+        }
+
+        close(server_socket_file_descriptor);
+    }
+
+    printf("Bound to socket...\n");
+
+    // Once we are bounded or are unable to bind, handle errs and tidy up
+    if(address_iterator == NULL){
+        fprintf(stderr, "Could not bind any addresses...\n");
+        exit(1);
+    }
+
+    freeaddrinfo(address);
+    printf("Freed address info...\n");
+
+    // Finally, mark the socket as passive (listen instead of making outbound conns)
+    listen(server_socket_file_descriptor, MAX_LISTEN_BACKLOG);
+    printf("Listening...\n");
+
+    // Now that we have a server sock ready, create an endless loop doing the proxying
+    while(1){
+        printf("Trying to define client socket file descriptor...\n");
+        client_socket_file_descriptor = accept(server_socket_file_descriptor,
+                                               NULL,
+                                               NULL);
+        printf("Client socket file descriptor defined...\n");
+
+        if(client_socket_file_descriptor == -1){
+            perror("Could not accept the socket...");
+            exit(1);
+        }
+
+        // Now we have a client file descriptor that describes a client conn. We can hand this off to the
+        // handle_client_connection func built earlier
+        printf("All good. Handling client connection...\n");
+        handle_client_connection(client_socket_file_descriptor,
+                                 backend_address,
+                                 backend_port_string);
+    }
 }
